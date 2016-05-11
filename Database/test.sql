655b01f338f774630d90eba60cde7018f28ab3d2
DjangoPeng <pjt73651@gmail.com>
2016-05-11 21:55:05 +0800

    Test SQL script

---
+++ Database/test.sql
@@ -0,0 +1,41 @@
+﻿SELECT *
+FROM selDiagnoses
+;
+
+SELECT *
+FROM report_1
+WHERE diagnosis='{sepsis}'
+;
+
+SELECT *
+FROM selMedications
+;
+
+SELECT *
+FROM testData
+ORDER BY subject_id
+LIMIT 100
+;
+
+SELECT *
+FROM report_1
+LIMIT 10
+;
+
+SELECT *
+FROM report_1
+WHERE subject_id = 305
+;
+
+SELECT *
+FROM trainData
+LIMIT 100
+;
+
+SELECT *
+FROM selEvents
+;
+
+SELECT *
+FROM selDiagnoses
+;
